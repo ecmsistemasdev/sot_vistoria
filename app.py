cbf18e06fb743f8fe9052fd1ca13439663a4d21b
ecmsistemasdev <164636970+ecmsistemasdev@users.noreply.github.com>
2025-10-28 10:14:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import uuid
 import base64
-from datetime import datetime
+from datetime import datetime, timedelta
 from io import BytesIO
 from pytz import timezone
 
